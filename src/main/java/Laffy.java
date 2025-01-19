3deb6d12f4ceb184563e5a02f40073c1c2642058
LUKAS\lohzh <lohzhenglucas@gmail.com>
2025-01-19 13:00:44 +0800

    Level 0. Rename, Greet, Exit (a) Give your chatbot a new name, to differentiate it from the placeholder name Duke. Avoid these common choices as well: Chatty, Jarvis, ChatBot, Chad
    (b) Implement an initial skeletal version of it that simply greets the user and exits.

---
+++ src/main/java/Laffy.java
@@ -0,0 +1,23 @@
+public class Laffy {
+
+    public static void horizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Laffy");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        horizontalLine();
+        greet();
+        horizontalLine();
+        bye();
+        horizontalLine();
+    }
+}

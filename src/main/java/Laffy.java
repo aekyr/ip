8993f9890b974395ad30958603bb09fe3cb2b5be
LUKAS\lohzh <lohzhenglucas@gmail.com>
2025-01-19 13:12:42 +0800

    Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Laffy.java
+++ src/main/java/Laffy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Laffy {
 
     public static void horizontalLine() {
@@ -5,19 +7,35 @@ public class Laffy {
     }
 
     public static void greet() {
-        System.out.println("Hello! I'm Laffy");
+        horizontalLine();
+        System.out.println("Hello! I'm L.A.F.F.Y");
         System.out.println("What can I do for you?");
+        horizontalLine();
     }
 
     public static void bye() {
+        horizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
+        horizontalLine();
+    }
+
+    public static void echo(String input) {
+        horizontalLine();
+        System.out.println(input);
+        horizontalLine();
     }
 
     public static void main(String[] args) {
-        horizontalLine();
         greet();
-        horizontalLine();
+        while (true) {
+            System.out.print("> ");
+            String input = new Scanner(System.in).nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                echo(input);
+            }
+        }
         bye();
-        horizontalLine();
     }
 }

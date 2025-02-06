ad3429811a38b9b757634b6a69b5270c9b846681
LUKAS\lohzh <lohzhenglucas@gmail.com>
2025-02-06 21:33:58 +0800

    Style gui to look nicer

--- src/main/java/laffy/gui/Main.java
+++ src/main/java/laffy/gui/Main.java
@@ -24,6 +24,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setLaffy(laffy);
             stage.show();
         } catch (IOException e) {
